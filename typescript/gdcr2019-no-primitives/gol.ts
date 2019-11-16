31e2de1693bc119b1234afe70817384065ebb88a
Adam Zielinski <github@maosmurf.com>
2019-11-16 12:04:52 +0100

    Alive cell should die when over-crowded

--- typescript/gdcr2019-no-primitives/gol.ts
+++ typescript/gdcr2019-no-primitives/gol.ts
@@ -5,10 +5,14 @@ export enum State {
 
 export enum NeighborState {
     SURVIVOR,
-    UNDER_POPULATION
+    UNDER_POPULATION,
+    OVER_CROWDING
 }
 
 export function nextState(state: State, neighborState: NeighborState): State {
+    if(neighborState == NeighborState.OVER_CROWDING) {
+        return State.DEAD;
+    }
     if(neighborState == NeighborState.UNDER_POPULATION) {
         return State.DEAD;
     }
